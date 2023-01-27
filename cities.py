85ddec3575b5a9f7e8b3f6ca3f2b8da59d858d7c
arromero4 <arromero0408@gmail.com>
2023-01-27 00:49:26 -0600

    Python Crash Course

---
+++ cities.py
@@ -0,0 +1,29 @@
+cities = {
+    'CDMX' : {
+      'country': 'Mexico',
+      'population': '8.8M',
+      'fact': 'Capital of the country'
+    },
+
+    'Brasilia': {
+      'country': 'Brasil',
+      'population': '4.7M',
+      'fact': 'Capital of the samba'
+      
+    },
+
+    'Roma': {
+      'country': 'Italy',
+      'population': '10M',
+      'fact': 'Capital of the Romance and The Colosseum'
+      
+    }
+}
+
+
+for city, info in cities.items():
+    print(f"{city}: ")
+    print(f"\tCountry: {info['country']}")
+    print(f"\tPopulation: {info['population']}")
+    print(f"\tFact: {info['fact']}")
+
