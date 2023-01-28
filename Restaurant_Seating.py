9d41a0edbd54a89d3a109375719d163e1a913acb
arromero4 <arromero0408@gmail.com>
2023-01-28 01:44:43 -0600

    Python Crash Course

---
+++ Restaurant_Seating.py
@@ -0,0 +1,6 @@
+people = int(input("how many people are in your dinner group?"))
+
+if people > 8:
+    print("You will have to wait for a table, please.")
+else:
+    print("Table is ready, please come in!")
---
+++ rental-cars.py
@@ -0,0 +1,2 @@
+car = input("What kind of car do you want to rent?")
+print(f"Let me see if I can ﬁnd you a {car}.")
