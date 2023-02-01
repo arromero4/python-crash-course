d1422432fca80e4cb5ac5da0c8cc266925d4d760
arromero4 <arromero0408@gmail.com>
2023-02-01 01:06:51 -0600

    Python Crash Course

---
+++ making_pizza_3.py
@@ -0,0 +1,5 @@
+#making_pizza_2.py
+import making_pizza_1 as p
+
+p.make_pizza(16, 'pepperoni')
+p.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
---
+++ making_pizza_4.py
@@ -0,0 +1,4 @@
+#making_pizza_2.py
+from making_pizza_1 import *
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
