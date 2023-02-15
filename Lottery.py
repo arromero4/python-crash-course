dab18cb2556345e98f2cf5cad7c7be81d9930b91
arromero4 <arromero0408@gmail.com>
2023-02-15 02:10:13 -0600

    Python Crash Course

--- Lottery.py
+++ Lottery.py
@@ -1,6 +1,66 @@
 from random import choice
-players = (1,2,3,4,5,6,7,8,9, 'A','B','C','D','E','F')
 
-chosen = [str(choice(players)) for x in range(4)]
-# print the four chosen items
-print(f"Any ticket matching {''.join(chosen)} wins a prize!")
+def get_winning_ticket(players):
+    """Return a winning ticket from a set of possibilities."""
+    winning_ticket = []
+    # We don't want to repeat winning numbers or letters
+    while len(winning_ticket) < 4:
+        pulled_item = choice(players)
+        if pulled_item not in winning_ticket:
+            winning_ticket.append(pulled_item)
+
+    return winning_ticket
+
+def check_ticket(played_ticket, winning_ticket):
+    # Check all elements in the played ticket. If any are not in the 
+    #   winning ticket, return False.
+    for element in played_ticket:
+        if element not in winning_ticket:
+            return False
+
+    # We must have a winning ticket!
+    return True
+
+def make_random_ticket(possibilities):
+    """Return a random ticket from a set of possibilities."""
+    ticket = []
+    # We don't want to repeat numbers or letters, so we'll use a while loop.
+    while len(ticket) < 4:
+        pulled_item = choice(possibilities)
+
+        # Only add the pulled item to the ticket if it hasn't already
+        #   been pulled.
+        if pulled_item not in ticket:
+            ticket.append(pulled_item)
+
+    return ticket
+
+
+players = (1,2,3,4,5,6,7,8,9, 'A','B','C','D','E','F')
+winning_ticket = get_winning_ticket(players)
+plays = 0
+won = False
+# Let's set a max number of tries, in case this takes forever!
+max_tries = 1_000_000
+
+while not won:
+    new_ticket = make_random_ticket(players)
+    won = check_ticket(new_ticket, winning_ticket)
+    plays += 1
+    if plays >= max_tries:
+        break
+
+if won:
+    print("We have a winning ticket!")
+    print(f"Your ticket: {new_ticket}")
+    print(f"Winning ticket: {winning_ticket}")
+    print(f"It only took {plays} tries to win!")
+else:
+    print(f"Tried {plays} times, without pulling a winner. :(")
+    print(f"Your ticket: {new_ticket}")
+    print(f"Winning ticket: {winning_ticket}")
+
+
+# chosen = [str(choice(players)) for x in range(4)]
+# # print the four chosen items
+# print(f"Any ticket matching {''.join(chosen)} wins a prize!")
