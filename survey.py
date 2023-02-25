ff530ba21e937c3c238a562c8d8f4398864475c7
arromero4 <arromero0408@gmail.com>
2023-02-25 02:46:50 -0600

    Python Crash Course

--- survey.py
+++ survey.py
@@ -4,12 +4,15 @@ class AnonymousSurvey:
         """Store a question, and prepare to store responses."""
         self.question = question
         self.responses = []
+        
     def show_question(self):
         """Show the survey question."""
         print(self.question)
+
     def store_response(self, new_response):
         """Store a single response to the survey."""
         self.responses.append(new_response)
+
     def show_results(self):
         """Show all the responses that have been given."""
         print("Survey results:")
