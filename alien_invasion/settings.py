dcd0f3750d7a5c4948ea2c8a90b1fd21c10f243d
arromero4 <arromero0408@gmail.com>
2023-03-02 20:05:04 -0600

    Python Crash Course Project Alien Invasion

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -3,8 +3,11 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings"""
         #screen settings
-        self.screen_width = 800
-        self.screen_height = 600
+        self.screen_width = 1280
+        self.screen_height = 720
         self.bg_color = (135, 206, 235)
+
+        #Ship settings
+        self.ship_speed = 1.5
 
         
