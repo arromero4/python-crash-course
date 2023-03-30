2ea91574c8292fcd1721b0a4de75e417a44389d3
arromero4 <arromero0408@gmail.com>
2023-03-30 02:00:45 -0600

    Python Crash Course - Alien Invasion Project

--- alien_invasion/game_stats.py
+++ alien_invasion/game_stats.py
@@ -6,7 +6,7 @@ class GameStats:
         self.reset_stats()
         
         # Start Alien Invasion in an active state.
-        self.game_active = False
+        self.game_active = True
     
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
