9e58376905c4f3e335bf983be5c8dd58ad20d49e
arromero4 <arromero0408@gmail.com>
2023-04-03 20:11:01 -0600

    Python Crash Course - Data Visualization Project

--- data_visualization/mpl_squares.py
+++ data_visualization/mpl_squares.py
@@ -2,10 +2,14 @@ import tkinter
 import matplotlib
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
+
+plt.style.use('seaborn')
+
 fig, ax = plt.subplots()
 
-ax.plot(squares, linewidth=3)
+ax.plot(input_values, squares, linewidth=3)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
