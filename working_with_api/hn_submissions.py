a5d50a3422eef9fdaf4783631baccbb648cefa2b
Andrés Reyes Romero <57555202+arromero4@users.noreply.github.com>
2023-04-09 14:27:49 -0600

    Update hn_submissions.py
    Adding comment

--- working_with_api/hn_submissions.py
+++ working_with_api/hn_submissions.py
@@ -1,5 +1,6 @@
 from operator import itemgetter
 import requests
+#Check import itemgetter
 
 # Make an API call and store the response.
 url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
